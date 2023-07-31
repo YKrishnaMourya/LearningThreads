97e92c9d1d3435dd1b4332f54968a8a21d3b68b3
Krishna Mourya <Krishna.Mourya@leoforce.local>
2023-07-31 19:50:42 +0530

    created Sample work method in console app

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+using System.Diagnostics;
+using System.Threading;
+
+void SampleWork()
+{
+    Console.WriteLine("Work Started");
+    Thread.Sleep(3000);
+    Console.WriteLine("Work Done");
+}
+
+var watch = Stopwatch.StartNew();
+
+SampleWork();
+SampleWork();
+
+watch.Stop();
+
+Console.WriteLine($"It took {watch.Elapsed.Seconds} second(s) to complete.");
